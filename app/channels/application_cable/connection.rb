050345e065ee503e900ebf7304b97390119a0aac
Vera Trade Refactor <debian@vps-ovh.local>
2026-04-19 19:34:56 +0000

    fix: add ApplicationCable base classes for ActionCable

---
+++ app/channels/application_cable/connection.rb
@@ -0,0 +1,21 @@
+module ApplicationCable
+  class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+
+    def connect
+      self.current_user = find_verified_user
+    end
+
+    private
+
+    def find_verified_user
+      if (verified_user = User.find_by(id: cookies.encrypted[:_vera_trade_session]["warden.user.user.key"]&.dig(0, 0)))
+        verified_user
+      else
+        reject_unauthorized_connection
+      end
+    rescue
+      reject_unauthorized_connection
+    end
+  end
+end
