4a3e467f066719cbd526705af492bc3cf6b98b37
Vera Trade Refactor <debian@vps-ovh.local>
2026-04-19 20:06:11 +0000

    fix: photo_url helper properly extracts blob for direct Cloudinary URLs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,10 +2,13 @@ module ApplicationHelper
   include Pagy::Frontend
 
   def photo_url(attachment)
-    return nil unless attachment&.attached?
-    blob = attachment.is_a?(ActiveStorage::Attached) ? attachment.blob : attachment
+    return nil if attachment.nil?
+    blob = case attachment
+           when ActiveStorage::Blob then attachment
+           when ActiveStorage::Attachment then attachment.blob
+           else attachment.respond_to?(:blob) ? attachment.blob : nil
+           end
+    return rails_blob_path(blob, only_path: true) unless blob&.service_name&.to_s == "cloudinary"
     blob.url
-  rescue
-    url_for(attachment)
   end
 end
